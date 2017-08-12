4d14ef682566c7ebffca9244a0f58c299ed7b09c
Simon <simon-github-public@think.io>
2017-08-12 12:21:33 +0100

    3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,16 @@
 class scriptspublic {
   
-  #$kernel_downcase = inline_template('<%= kernel.downcase %>')
-  
+  $kernel_downcase = downcase($kernel)
+
   file { "/usr/local/scripts/public/":
     owner 	=> "root",
     group 	=> "root",
-    mode  	=> "0775",
+    mode  	=> 0775,
   	purge   => true,
   	recurse => true,
   	force   => true,
   	ensure  => present,
   	ignore  => ".svn",
-  	source  => "puppet:///modules/scriptspublic/default/linux"
+  	source  => "puppet:///modules/scriptspublic/default/$kernel_downcase"
   }
 }
