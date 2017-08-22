77fc3d813f677b5f0ef7fd2cb5b78001f047d365
Simon <simon-github-public@think.io>
2017-08-22 21:00:23 +0100

    1

--- files/default/linux/bluetooth/screen_lock.sh
+++ files/default/linux/bluetooth/screen_lock.sh
@@ -3,12 +3,14 @@ export NOEXITONERROR="TRUE"
 . /usr/local/scripts/public/stdlib.sh
 
 BLUMAC=$1
-NEARBY=`hcitool cc $BLUMAC && hcitool auth $BLUMAC && hcitool dc $BLUMAC`
+NEARBY=`hcitool cc $BLUMAC && hcitool auth $BLUMAC && hcitool dc $BLUMAC; echo $?`
 
 if [ $NEARBY -eq 0 ]
 then
+  echo Phone is near
   sudo -u pi kill `ps aux |grep "xscreensaver" |grep -v grep |awk '{print $2}'`
 else
+  echo Phone is away
   sudo -u pi xscreensaver &
   sleep 1
   sudo -u pi xscreensaver-command -lock
