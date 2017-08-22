c114c7ffd1e8475a7298867987689f38aa58373d
Simon <simon-github-public@think.io>
2017-08-22 22:09:21 +0100

    ssh

---
+++ files/default/linux/iptables/block-all-except-ssh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+IPT="/sbin/iptables"
+
+$IPT --flush
+$IPT -F
+$IPT -X
+$IPT -t nat -F
+$IPT -t nat -X
+
+$IPT -A INPUT -i lo -j ACCEPT
+$IPT -A OUTPUT -o lo -j ACCEPT
+
+#Accept
+$IPT -A INPUT -p icmp -j ACCEPT
+$IPT -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+#SSH
+$IPT -A INPUT -p tcp --dport 22  -m state --state NEW,ESTABLISHED -j ACCEPT
+
+#Drop
+$IPT -A INPUT -j DROP
