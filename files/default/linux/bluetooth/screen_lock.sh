8db93a2c7d25d7c9cc84e3152f160e1a523deece
Simon <simon-github-public@think.io>
2017-08-22 21:18:31 +0100

    invert logic, i'm awake

--- files/default/linux/bluetooth/screen_lock.sh
+++ files/default/linux/bluetooth/screen_lock.sh
@@ -3,23 +3,21 @@ export NOEXITONERROR="TRUE"
 . /usr/local/scripts/public/stdlib.sh
 
 BLUMAC=$1
-NEARBY=`hcitool cc $BLUMAC && hcitool auth $BLUMAC && hcitool dc $BLUMAC; echo $?`
+NEARBY=`/usr/bin/hcitool cc $BLUMAC && /usr/bin/hcitool auth $BLUMAC && /usr/bin/hcitool dc $BLUMAC; echo $?`
 
 SCRSAV=`sudo -u pi ps aux |grep "xscreensaver" |grep -v grep |awk '{print $2}'`
 
 if [ $NEARBY -eq 0 ]
 then
   echo Phone is near
-  if [ -z "$SCRSAV" ]
+  if [ ! -z "$SCRSAV" ]
   then
     echo killing screensaver
     sudo -u pi kill $SCRSAV
   fi
 else
   echo Phone is away
-  echo $SCRSAV
-  echo ====
-  if [ ! -z "$SCRSAV" ]
+  if [ -z "$SCRSAV" ]
   then
     echo starting screensaver
     sudo -u pi xscreensaver &
