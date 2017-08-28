8f000f2e5570ec9d3244fa025cf2d8ca7aa7a519
Simon <simon-github-public@think.io>
2017-08-28 13:03:19 +0100

    different logging

--- files/default/linux/bluetooth/screen_lock.sh
+++ files/default/linux/bluetooth/screen_lock.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export NOEXITONERROR="TRUE"
-export LOGTO="FILE"
 . /usr/local/scripts/public/stdlib.sh
 
 BLUMAC=$1
@@ -10,23 +9,19 @@ SCRSAV=`sudo -u pi ps aux |grep "xscreensaver" |grep -v grep |awk '{print $2}'`
 
 if [ $NEARBY -eq 0 ]
 then
-  echo Phone is near
   if [ ! -z "$SCRSAV" ]
   then
+    echo Phone is near
     echo killing screensaver
     sudo -u pi kill $SCRSAV
-  else 
-    echo Already unlocked..
   fi
 else
-  echo Phone is away
   if [ -z "$SCRSAV" ]
   then
+    echo Phone is away
     echo starting screensaver
     sudo -u pi xscreensaver &
     sleep 1
     sudo -u pi xscreensaver-command -lock
-  else
-    echo Still locked...
   fi
 fi
