016463179d5a1533a1bb270451ef515fcf51e2c7
Simon <simon-github-public@think.io>
2018-05-26 20:47:33 +0100

    mysql backup

---
+++ files/default/linux/mysql/dump_all_regular.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+. /usr/local/scripts/public/stdlib.sh
+
+HOSTN=$1
+USERN=$2
+PASSW=$3
+DIREC=$4
+
+mkdir -p $DIREC/day
+mkdir -p $DIREC/week
+mkdir -p $DIREC/month
+
+/usr/bin/mysqldump -h $HOSTN -u $USERN -p $PASSW --all-databases > $DIREC/day/mysql_dump.sql
+
+if [ `date '+%u'` -eq 0 ]
+then
+  echo "Starting Week Backup at " `date`
+
+  /usr/bin/rsync $DIREC/day/ $DIREC/week/ -avz --delete-delay
+  echo "Stoping Month Backup at " `date`
+
+fi
+
+if [ `date '+%d'` -eq 1 ]
+then
+  echo "Starting Month Backup at " `date`
+  /usr/bin/rsync $DIREC/day/ $DIREC/month/ -avz --delete-delay
+  echo "Stoping Month Backup at " `date`
+fi
